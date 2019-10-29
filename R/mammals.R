cc9026f4cbe922cefb873642a6a70aad5ef1ea53
Nicolas Casajus <nicolas.casajus@gmail.com>
2019-10-29 16:25:53 +0100

    Add sample dataset documentation

---
+++ R/mammals.R
@@ -0,0 +1,23 @@
+#' @title Example dataset for popbayes package
+#'
+#' @description ...
+#'
+#' @docType data
+#'
+#' @usage data(mammals)
+#'
+#' @format A data frame with 69 rows and 11 variables:
+#'   - location: protected area name
+#'   - species: species name
+#'   - year: year
+#'   - counts: species counts
+#'   - cinf: CI95% lower bound
+#'   - csup: CI95% upper bound
+#'   - stat_method: S(ample), T(otal) or G(uesstimate)
+#'   - field_method: A(erial) or T(errestrial)
+#'   - cv: coefficient of variation of species count
+#'   - sd: standard deviation of species count
+#'   - var: variance of species count
+#'
+#' @source \url{https://www.github.com/frbcesab/popbayes}
+"mammals"
