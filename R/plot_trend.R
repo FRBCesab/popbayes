6a7d8573a75d7c853502fa241140c13348f07b0b
Nicolas Casajus <nicolas.casajus@gmail.com>
2019-10-30 10:27:30 +0100

    Add Plot function + Documentation

---
+++ R/plot_trend.R
@@ -0,0 +1,22 @@
+#' @title Plot BUGS model preditions
+#'
+#' @description
+#' ...
+#'
+#' @param model [string] a BUGS model obtained after running \code{popbayes::fit_trend()}.
+#'
+#' @author Nicolas CASAJUS, \email{nicolas.casajus@@fondationbiodiversite.fr}
+#' @author Roger PRADEL, \email{roger.pradel@@cefe.cnrs.fr}
+#'
+#' @export
+#'
+#' @return
+#' ...
+#'
+#' @examples
+#'
+#' # No example
+
+
+
+plot_trend <- function(model) { }
