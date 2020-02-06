a2a31cd614d89a9a5cb607756cf7adcef2298e8c
Roger PRADEL <roger.pradel@cefe.cnrs.fr>
2020-02-06 11:07:58 +0200

    minimal comments of function check_data

--- R/check_data.R
+++ R/check_data.R
@@ -1,10 +1,12 @@
-#' @title ...
+#' @title check count data
 #'
 #' @description
-#' ...
+#' To be usable for the estimation of population trend, count data must be accompanied by dates collected 
+#' and information of precision. This function checks that the data have the necessary information and are 
+#' in a suitable format for the function fit_trend.
 #'
-#' @param data [data frame] ...
-#' @param conversion_fac [numeric] ...
+#' @param data [data frame] data to be analysed (contains at the minimal counts, dates and count precision)
+#' @param conversion_fac [numeric] multiplicative factor to apply to ground counts n order to obtain equivalent aerial counts
 #'
 #' @author Nicolas CASAJUS, \email{nicolas.casajus@@fondationbiodiversite.fr}
 #' @author Roger PRADEL, \email{roger.pradel@@cefe.cnrs.fr}
@@ -30,7 +32,7 @@ check_data <- function(
     "stat_method", "field_method",
     "location", "species", "year", "counts"
   )
-
+# remove 'which' in following line?
   if (sum(which(colnames(data) %in% col_names)) < length(col_names)) {
 
     stop("...")
